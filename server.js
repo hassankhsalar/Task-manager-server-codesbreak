ffa13a62d6d776c5d088dfee98be40212a75837e
Hassan kh salar <khsalarhassan@gmail.com>
2025-08-18 01:54:19 +0600

    added controller functions

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes")
 const userRoutes = require("./routes/userRoutes")
+const taskRoutes = require("./routes/taskRoutes")
 
 const app = express();
 //middleware for handling cors
@@ -29,7 +30,7 @@ app.use(express.json());
 
  app.use("/api/auth", authRoutes);
  app.use("/api/users", userRoutes);
-// app.use("/api/tasks", taskRoutes);
+ app.use("/api/tasks", taskRoutes);
 // app.use("/api/reports", reportRoutes);
 
 
